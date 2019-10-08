ad1b01b17b957a502da5c50aae7334722fe17d08
srfnd <fndsantos23@gmail.com>
2019-10-08 13:48:21 -0400

    first commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    appID: 'type your appID',
+    apiKey: 'type your api key',
+  };
+
+  export default config;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './pages/App';
+import {CometChat} from '@cometchat-pro/chat';
+import config from './config';
+import './globalStyle.css';
+
+CometChat.init(config.appID);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
